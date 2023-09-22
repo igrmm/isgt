5bef11e98c3108b6e06b08995d787cd236ae6e2e
igrmm <igrmmdev@gmail.com>
2023-09-22 12:33:10 -0400

    add sdl_ttf to android script

--- install_android.sh
+++ install_android.sh
@@ -12,6 +12,12 @@ if [ ! -d "android" ]; then
     mv ../../../SDL ./
     git clone https://github.com/libsdl-org/SDL_image.git
     cd SDL_image && git checkout tags/release-2.6.3 && cd ../
+    git clone https://github.com/libsdl-org/SDL_ttf.git
+    cd SDL_ttf && git checkout tags/release-2.20.2 && cd external
+    git clone https://github.com/libsdl-org/freetype.git
+    cd freetype && git checkout fd08bbe && cd ../
+    git clone https://github.com/libsdl-org/harfbuzz.git
+    cd harfbuzz && git checkout c248ad5 && cd ../../../
 
     echo \
         'LOCAL_PATH := $(call my-dir)
@@ -22,11 +28,11 @@ if [ ! -d "android" ]; then
 
     SDL_PATH := ../SDL
 
-    LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL_image/
+    LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL_image/ $(LOCAL_PATH)/../SDL_ttf/
 
     LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../../../../../src/*.c)
 
-    LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
+    LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf
 
     LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 
