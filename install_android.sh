19efdbccf6d89770083a002116375fdef0e23248
igrmm <igrmmdev@gmail.com>
2023-09-23 13:22:56 -0400

    add variables for sdl,sdl_image and sdl_ttf commits for git checkout

--- install_android.sh
+++ install_android.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
+SDL_COMMIT=tags/release-2.28.3
+SDL_IMAGE_COMMIT=tags/release-2.6.3
+SDL_TTF_COMMIT=tags/release-2.20.2
+
 mkdir -p build
-cd build
 
 if [ ! -d "android" ]; then
+    cd build
     mkdir -p android && cd android
     git clone https://github.com/libsdl-org/SDL/
-    cd SDL && git checkout tags/release-2.28.3
+    cd SDL && git checkout $SDL_COMMIT
     mv android-project ../ && cd ../
     cd android-project/app/jni/
     mv ../../../SDL ./
     git clone https://github.com/libsdl-org/SDL_image.git
-    cd SDL_image && git checkout tags/release-2.6.3 && cd ../
+    cd SDL_image && git checkout $SDL_IMAGE_COMMIT && cd ../
     git clone https://github.com/libsdl-org/SDL_ttf.git
-    cd SDL_ttf && git checkout tags/release-2.20.2 && cd external
-    git clone https://github.com/libsdl-org/freetype.git
-    cd freetype && git checkout fd08bbe && cd ../
-    git clone https://github.com/libsdl-org/harfbuzz.git
-    cd harfbuzz && git checkout c248ad5 && cd ../../../
+    cd SDL_ttf && git checkout $SDL_TTF_COMMIT && git submodule update --init
+    cd ../
 
     echo \
         'LOCAL_PATH := $(call my-dir)
@@ -42,7 +43,6 @@ if [ ! -d "android" ]; then
     cd ../../../../../
     ln -s $(pwd)/assets build/android/android-project/app/src/main/
 fi
-
 cd build/android/android-project/
 ./gradlew installDebug
 cd ../../../
